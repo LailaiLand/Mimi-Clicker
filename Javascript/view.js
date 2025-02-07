a8f8e5fc8b6ba707b53a78cf0d5af4740b15f73c
Nikolaipe <nikolaipe@getacademy.no>
2025-02-07 04:16:13 +0100

    Messing with Grid
    Grid is hard...

--- Javascript/view.js
+++ Javascript/view.js
@@ -1,9 +1,11 @@
 test();
 
 function test() {
-    sampleString = "";
+  sampleString = "";
   for (let i = 0; i < 20; i++) {
-    sampleString += '<img class="test_item" src="HaruEmotes/Dohya.png">';
+    if (i == 1) {
+      sampleString += '<img class="test_item" src="HaruEmotes/Dohya.png">';
+    } else sampleString += '<div class="test_item"></div>';
   }
   model.view.innerHTML += `<div class="page">${sampleString}</div>`;
 }
